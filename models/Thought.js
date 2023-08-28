fbb840cc5072f900b3f62923c901e0979793f3c8
kyungkwon01 <kyungkwon01@gmail.com>
2023-08-28 14:49:41 -0600

    added models and moment module

--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,12 @@
 const { Schema, model } = require('mongoose');
+const moment = require('moment');
 
-const thoughtSchema = new Schema(
+const reactionSchema = new Schema(
 	{
+		reactionId: {
+			type: Schema.Types.ObjectId,
+			default: () => new Types.ObjectId(),
+		},
 		thoughtText: {
 			type: String,
 			required: true,
@@ -13,18 +18,16 @@ const thoughtSchema = new Schema(
 			unique: true,
 			match: [/.+@.+\..+/],
 		},
-		username: [
-			{
-				type: Schema.Types.ObjectId,
-				ref: 'Thought',
-			},
-		],
-		reactions: [
-			{
-				type: Schema.Types.ObjectId,
-				ref: 'User',
-			},
-		],
+		username: {
+			type: String,
+			required: true,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			get: (createdAtVal) =>
+				moment(createdAtVal).format('MMM DD, YY [at] hh:mm a'),
+		},
 	},
 	{
 		toJSON: {
@@ -33,6 +36,40 @@ const thoughtSchema = new Schema(
 		id: false,
 	}
 );
+
+const thoughtSchema = new Schema(
+	{
+		thoughtText: {
+			type: String,
+			required: 'Thought text is required',
+			minlength: 1,
+			maxlength: 280,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			get: (createdAtVal) =>
+				moment(createdAtVal).format('MMM DD, YY [at] hh:mm a'),
+		},
+		username: {
+			type: String,
+			required: true,
+		},
+		reactions: [reactionSchema],
+	},
+
+	{
+		toJSON: {
+			virtuals: true,
+			getters: true,
+		},
+		id: false,
+	}
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+	return this.reaction.length;
+});
 
 const Thought = model('thought', thoughtSchema);
 
